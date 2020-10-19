cb79e14ea28027452ae1a51bb1adde1296b67ae1
Ziyak Jehangir <ziyak97@gmail.com>
2020-10-19 22:48:44 +0530

    Added services from backend on homepage.

--- pages/index.js
+++ pages/index.js
@@ -6,31 +6,36 @@ import World from '../components/world/world.component'
 import Growth from '../components/growth/growth.component'
 
 export default function Home(props) {
-  const { testimonials } = props
-  return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <MainSection />
-        <SupportCard />
-        <Testimonial testimonials={testimonials} />
-        <World />
-        <Growth />
-      </main>
-    </div>
-  )
+	const { testimonials, cards } = props
+	return (
+		<div className={styles.container}>
+			<main className={styles.main}>
+				<MainSection />
+				<SupportCard cards={cards} />
+				<Testimonial testimonials={testimonials} />
+				<World />
+				<Growth />
+			</main>
+		</div>
+	)
 }
 
 export async function getStaticProps() {
-  const { API_URL } = process.env
+	const { API_URL } = process.env
 
-  const resTestimonials = await fetch(`${API_URL}/testimonials`)
-  let testimonials = await resTestimonials.json()
-  testimonials = testimonials.sort((a, b) => a.Position - b.Position)
+	const resTestimonials = await fetch(`${API_URL}/testimonials`)
+	let testimonials = await resTestimonials.json()
+	testimonials = testimonials.sort((a, b) => a.Position - b.Position)
 
-  return {
-    props: {
-      testimonials,
-    },
-    revalidate: 1,
-  }
+	const resCards = await fetch(`${API_URL}/services-cards`)
+	let cards = await resCards.json()
+	cards = cards.sort((a, b) => a.Position - b.Position)
+
+	return {
+		props: {
+			testimonials,
+			cards,
+		},
+		revalidate: 1,
+	}
 }
